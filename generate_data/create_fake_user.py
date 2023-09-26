89af9fcd25f9348ed8dc3cba16ff6f59dbeb4384
Duong <hoatungduong12@gmail.com>
2023-09-26 09:33:04 +0700

    COOKK

--- generate_data/create_fake_user.py
+++ generate_data/create_fake_user.py
@@ -6,23 +6,22 @@ import string
 server_url = "http://localhost:8000/user_service/user/create"
 user_number=20
 # Function to generate random data for the user
-def generate_random_user_data(user_id):
+def generate_random_user_data():
     username = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
     password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=12))
-    gender = random.choice(['M', 'F'])
+    gender = random.choice(['Male', 'Female','Other'])
     date_of_birth = f"19{random.randint(60, 99)}-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}"
     phone_number = ''.join(random.choices(string.digits, k=10))
     balance = round(random.uniform(0, 1000), 2)
     email = f"{username}@example.com"  # Generate a random email based on the username
     user_data = {
-        "user_id": user_id,
-        "username": username,
-        "password": password,
-        "email": email,
-        "gender": gender,
-        "date_of_birth": date_of_birth,
-        "phone_number": phone_number,
-        "balance": balance,
+        "Username": username,
+        "Password": password,
+        "Email": email,
+        "Gender": gender,
+        "DateOfBirth": date_of_birth,
+        "PhoneNumber": phone_number,
+        "Balance": balance,
     }
     return user_data
 
